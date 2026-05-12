631460a6ad0f2b6cd4695c5af56decee11e068e1
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-05-12 11:59:09 -0300

    Teste de integração
    Teste de integração da solution IncluirDadosTemplateHtml

---
+++ back-end-create-curriculum/src/tests/integration/subdomains/templates/domain/services/incluirDadosTemplateHtml.spec.ts
@@ -0,0 +1,28 @@
+import { DadosTemplate } from "@templates/types/dadosTemplate";
+import { selecionarTemplateHTMLService } from "@templates/domain/services/selecionarTemplateHtml.service";
+import { incluirDadosTemplateHtmlService } from "@templates/domain/services/incluirDadosTemplateHtml.service";
+import { mockDadosTemplate } from "@/tests/mocks/subdomains/templates/domain/entity/template.mock";
+import { TemplatePadrao } from "@/subdomains/templates/domain/entity/templates.entity";
+
+describe("incluirDadosTempalteHtml", () => {
+  test("Verificar se é feita a inclusão dos dados do usuário no template html/css", async () => {
+    const { template: dadosUsuario, tipo } = mockDadosTemplate();
+
+    const template = await selecionarTemplateHTMLService(tipo);
+
+    if (!template.html.includes("{nome}"))
+      throw new Error("Falha na geração do campo name no template!");
+
+    const service = incluirDadosTemplateHtmlService(
+      dadosUsuario as TemplatePadrao,
+      tipo,
+      template,
+    );
+
+    expect(typeof service).toBe("string");
+    expect(service).toContain("<html");
+    expect(service).toContain("<style");
+    expect(service).not.toContain("{nome}");
+    expect(service).toContain(dadosUsuario.nome);
+  });
+});
