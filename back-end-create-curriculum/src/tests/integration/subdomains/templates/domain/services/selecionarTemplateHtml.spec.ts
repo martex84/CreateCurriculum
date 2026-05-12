081c6646a96655ba2013cef088d1c9c9fec85cb2
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-05-12 11:59:28 -0300

    Teste de integração
    Teste de integração da solution selecionarTemplateHtml

---
+++ back-end-create-curriculum/src/tests/integration/subdomains/templates/domain/services/selecionarTemplateHtml.spec.ts
@@ -0,0 +1,24 @@
+import { selecionarTemplateHTMLService } from "@/subdomains/templates/domain/services/selecionarTemplateHtml.service";
+import { TiposTemplates } from "@/subdomains/templates/types/tiposTemplates";
+import { mockDadosTemplate } from "@/tests/mocks/subdomains/templates/domain/entity/template.mock";
+
+describe("SelecionarTempalteHtml", () => {
+  test("Verificar se é realizado a seleção do template html e de css com base ao tipo informado", async () => {
+    const { template, tipo } = mockDadosTemplate();
+
+    const campoUtilizado = Object.keys(template)[0];
+
+    const verificacao = selecionarTemplateHTMLService(tipo);
+
+    expect(await verificacao).toHaveProperty("html");
+    expect(await verificacao).toHaveProperty("css");
+    expect(typeof (await verificacao).html).toBe("string");
+    expect(typeof (await verificacao).css).toBe("string");
+    expect((await verificacao).html).toContain("<html");
+    expect((await verificacao).html).toContain("<head");
+    expect((await verificacao).html).toContain("<body");
+    expect((await verificacao).html).not.toHaveLength(0);
+    expect((await verificacao).css).not.toHaveLength(0);
+    expect((await verificacao).html).toContain(`{${campoUtilizado}}`);
+  });
+});
