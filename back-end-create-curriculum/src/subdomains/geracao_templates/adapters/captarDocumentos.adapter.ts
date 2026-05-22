70281465f680f234f20bd76a978375096f4748b4
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-05-22 11:19:08 -0300

    Inclusão de adapter
    Inclusão do adapter "CaptarDocumentos"

---
+++ back-end-create-curriculum/src/subdomains/geracao_templates/adapters/captarDocumentos.adapter.ts
@@ -0,0 +1,44 @@
+import fs from "node:fs";
+import { TiposTemplates } from "@/subdomains/geracao_templates/types/tiposTemplates";
+import { DadosTemplate } from "@templates/types/dadosTemplate";
+import { GenerationError } from "@/shared/errors/generation-error";
+import { erros } from "./captarDocumentos.error";
+import { ITemplatesCaptarDocumentosAdapterPort } from "@/subdomains/geracao_templates/ports/iTemplatesCaptarDocumentosAdapterPort";
+
+export class CaptarDocumentosAdapter implements ITemplatesCaptarDocumentosAdapterPort {
+  constructor(private readonly localArquivo: string) {}
+
+  async captarHtmlCSS(tipoTemplate: TiposTemplates) {
+    try {
+      if (!tipoTemplate) throw new Error(erros.erroRecebimentoTipoTemplate);
+
+      let dadosTemplate: DadosTemplate = {
+        html: "",
+        css: "",
+      };
+
+      let nomeLocal: string;
+
+      if (tipoTemplate === "padrao") {
+        nomeLocal = "padrao";
+
+        dadosTemplate.html = fs
+          .readFileSync(this.localArquivo + nomeLocal + "/" + "index.html")
+          .toString();
+
+        dadosTemplate.css = fs
+          .readFileSync(this.localArquivo + nomeLocal + "/" + "style.css")
+          .toString();
+      } else throw new Error(erros.erroTipoTemplateInvalido);
+
+      if (dadosTemplate.html === "" || dadosTemplate.css === "")
+        throw new Error("Falha ao tentar preencher os dados do template!");
+
+      return dadosTemplate;
+    } catch (error: any) {
+      throw new GenerationError(
+        `Falha na captação dos dados do template : [${error.message}]`,
+      );
+    }
+  }
+}
---
+++ back-end-create-curriculum/src/subdomains/geracao_templates/adapters/captarDocumentos.error.ts
@@ -0,0 +1,4 @@
+export const erros = {
+  erroRecebimentoTipoTemplate: "Falha ao receber o tipo de template!",
+  erroTipoTemplateInvalido: "Falha ao tentar localizar o tipo de template",
+};
