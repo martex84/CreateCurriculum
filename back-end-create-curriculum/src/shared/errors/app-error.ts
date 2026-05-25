2d3596679bde0267cf2383fd8016f2a3e68a3bc0
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-05-25 16:31:25 -0300

    Atualização Classe
    Atualização das classes de erro para conter a propriedade de detalhes

--- back-end-create-curriculum/src/shared/errors/app-error.ts
+++ back-end-create-curriculum/src/shared/errors/app-error.ts
@@ -3,6 +3,7 @@ export class AppError extends Error {
     public readonly message: string,
     public readonly statusCode: number = 400,
     public readonly isCustomError: boolean = true,
+    public readonly detalhes: any = null,
   ) {
     super(message);
     this.name = this.constructor.name;
