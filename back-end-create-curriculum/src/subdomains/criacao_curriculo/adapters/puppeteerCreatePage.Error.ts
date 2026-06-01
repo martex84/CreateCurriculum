990f58a1d20917e31716c6d0339d409e310dce1b
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-06-01 17:12:33 -0300

    Criação de adapter
    Criação do adapter "puppeteercreatePage", juntamente com seu teste de integração

---
+++ back-end-create-curriculum/src/subdomains/criacao_curriculo/adapters/puppeteerCreatePage.Error.ts
@@ -0,0 +1,5 @@
+export const erros = {
+  falhaLocalizacaoDadosPagina: "Falha ao tentar localizar os dados da página!",
+  falhaFechamentoPagina: "Falha ao tentar fechar a página!",
+  falhaPreencherArquivoPdf: "Falha ao tentar preencher o arquivo pdf!",
+};
