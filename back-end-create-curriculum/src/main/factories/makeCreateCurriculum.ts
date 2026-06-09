e1419f92665bd5745b9941f43d4b0c992795576a
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-06-09 17:26:47 -0300

    Criação Teste Integração
    Criação do teste de integração do useCase "CriarCurriculum"

--- back-end-create-curriculum/src/main/factories/makeCreateCurriculum.ts
+++ back-end-create-curriculum/src/main/factories/makeCreateCurriculum.ts
@@ -1,14 +1,25 @@
 import { CriarCurriculumUseCase } from "@criacao_curriculo/useCase/criarCurriculum.useCase";
-import { PuppeteerCurriculumAdapter } from "@criacao_curriculo/adapters/puppeteerCurriculumAdapter";
+import { PuppeteerCreatePage } from "@criacao_curriculo/adapters/puppeteerCreatePage.Adapter";
+import { CriacaoLogs } from "@/config/log";
+import path from "node:path";
+import { FsCreatePDF } from "@/subdomains/criacao_curriculo/adapters/fsCreatePdf.Adapter";
 
 export interface MakeCreateCurriculum {
   criarCurriculumUseCase: CriarCurriculumUseCase;
 }
 
 export const makeCreateCurriculum = (): MakeCreateCurriculum => {
-  const puppeteerCurriculumAdapter = new PuppeteerCurriculumAdapter();
+  const localPasta = path.join(process.cwd(), "temp");
+
+  const criacaoLogs = new CriacaoLogs();
+
+  const puppeteerCreatePage = new PuppeteerCreatePage();
+  const createPdf = new FsCreatePDF(localPasta);
+
   const criarCurriculumUseCase = new CriarCurriculumUseCase(
-    puppeteerCurriculumAdapter,
+    puppeteerCreatePage,
+    createPdf,
+    criacaoLogs,
   );
 
   return {
--- back-end-create-curriculum/src/subdomains/criacao_curriculo/useCase/criarCurriculum.useCase.ts
+++ back-end-create-curriculum/src/subdomains/criacao_curriculo/useCase/criarCurriculum.useCase.ts
@@ -1,14 +1,65 @@
+import { Logs } from "@/shared/types/logs";
 import { Curriculum } from "@criacao_curriculo/domain/entity/curriculum.entity";
-import { ICurriculumPort } from "@criacao_curriculo/ports/iCurriculumPort";
+import { PuppeteerCreatePage } from "@criacao_curriculo/adapters/puppeteerCreatePage.Adapter";
+import { FsCreatePDF } from "@/subdomains/criacao_curriculo/adapters/fsCreatePdf.Adapter";
 
 export class CriarCurriculumUseCase {
-  constructor(private readonly ICurriculumPort: ICurriculumPort) {}
+  constructor(
+    private readonly puppeteerCreatePage: PuppeteerCreatePage,
+    private readonly fsCreatePDF: FsCreatePDF,
+    private readonly log: Logs,
+  ) {}
 
   async execution(html: string): Promise<Curriculum> {
+    let curriculum: Curriculum = {
+      arquivo: "",
+      error: {
+        isError: false,
+        messageError: "",
+      },
+    };
+
     try {
-      return await this.ICurriculumPort.criarCurriculum(html);
+      this.log.execution("Inicio da criação de logs");
+
+      this.log.execution("Criação da página");
+
+      //Realiza a criação da página e capta seus dados
+      await this.puppeteerCreatePage.createPage(html);
+
+      this.log.execution("Criação do arquivo em branco");
+
+      const dadosArquivo = await this.fsCreatePDF.geracaoArquivo();
+
+      this.log.execution("Preenchimento do pdf");
+
+      await this.puppeteerCreatePage.preencherArquivoPdf(
+        dadosArquivo.localArquivo,
+      );
+
+      this.log.execution("Pdf Preenchido com sucesso!");
+
+      this.log.execution("Conversão do arquivo pdf para string");
+
+      const dadosPdf = await this.fsCreatePDF.converterArquivoPdf();
+
+      curriculum.arquivo = dadosPdf;
+
+      this.log.execution(
+        `O arquivo foi convertido com sucesso, trazendo ao total ${curriculum.arquivo.length} bytes!`,
+      );
+
+      this.log.execution("Curriculo criado com sucesso!");
+
+      this.log.execution("Encerramento do browser");
+
+      return curriculum;
     } catch (error) {
       throw new Error(`Falha na geração do curriculum: ${error}`);
+    } finally {
+      await this.fsCreatePDF.apagarArquivo();
+
+      await this.puppeteerCreatePage.closeBrowser();
     }
   }
 }
